ddb6176feb8e95b20c249b62bc8308aad39ff064
omar khaled <omarkhaled10102004@gmail.com>
2026-07-24 22:30:33 +0300

    feat(auth): Refactor local data sources to use SecureStorageService for token management and add unit tests for SecureStorageService

--- lib/features/auth/api/data_sources/local/logout_local_data_source_impl.dart
+++ lib/features/auth/api/data_sources/local/logout_local_data_source_impl.dart
@@ -1,19 +1,17 @@
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:injectable/injectable.dart';
 import 'package:tribe_up/config/base_response/base_response.dart';
-import 'package:tribe_up/core/constants/api_constants.dart';
+import 'package:tribe_up/core/services/secure_storage_service.dart';
 import 'package:tribe_up/features/auth/data/data_sources/local/logout_local_data_source.dart';
 
 @Injectable(as: LogoutLocalDataSource)
 class LogoutLocalDataSourceImpl implements LogoutLocalDataSource {
-  final Box<String> _box;
+  final SecureStorageService _secureStorageService;
 
-  LogoutLocalDataSourceImpl(this._box);
+  LogoutLocalDataSourceImpl(this._secureStorageService);
 
   @override
   Future<BaseResponse<void>> clearTokens() async {
-    await _box.delete(CacheConstants.tokenKey);
-    await _box.delete(CacheConstants.refreshTokenKey);
+    await _secureStorageService.clearAuthData();
     return SuccessResponse<void>(data: null);
   }
 }
